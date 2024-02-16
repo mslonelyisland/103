88bfb59288884d115b8014dd67b4f29eaf2a548f
Weluna-Rose <welunarose123@gmail.com>
2024-02-16 19:36:51 +0800

    new user info

--- User.js
+++ User.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+// User Schema
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String,
-    age: Number,
-})
+    club: { type: mongoose.Schema.Types.ObjectId, ref: 'clubs' }, // Foreign key reference to Club model
+    position: String,
+});
 
-const UserModel = mongoose.model("users", UserSchema)
+const UserModel = mongoose.model("users", UserSchema);
+
 module.exports = UserModel;
